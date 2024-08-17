9255df0bfb38dce4f424bfad11a00d5585c0fd9b
Ricardo <ricardo.mery@alumnos.usm.cl>
2024-08-17 11:07:45 -0400

    Admin y estilos 2.0

--- admin.js
+++ admin.js
@@ -5,13 +5,17 @@ document.getElementById('training-form').addEventListener('submit', function(e) 
     const day = document.getElementById('day').value.toLowerCase();
     const session = document.getElementById('session').value;
     const exercise1 = document.getElementById('exercise1').value;
+    const exercise1Video = document.getElementById('exercise1-video').value;
     const exercise2 = document.getElementById('exercise2').value;
+    const exercise2Video = document.getElementById('exercise2-video').value;
     const exercise3 = document.getElementById('exercise3').value;
+    const exercise3Video = document.getElementById('exercise3-video').value;
 
     // Crear un objeto de entrenamiento
     const trainingData = {
         session,
-        exercises: [exercise1, exercise2, exercise3]
+        exercises: [exercise1, exercise2, exercise3],
+        videos: [exercise1Video, exercise2Video, exercise3Video]
     };
 
     // Guardar en localStorage
--- scripts.js
+++ scripts.js
@@ -6,8 +6,11 @@ window.onload = () => {
         if (trainingData) {
             document.getElementById(`${day}-session`).textContent = trainingData.session || 'No disponible';
             document.getElementById(`${day}-exercise1`).textContent = trainingData.exercises[0] || 'No disponible';
-            document.getElementById(`${day}-exercise2}`).textContent = trainingData.exercises[1] || 'No disponible';
-            document.getElementById(`${day}-exercise3}`).textContent = trainingData.exercises[2] || 'No disponible';
+            document.getElementById(`${day}-exercise1-video`).href = trainingData.videos[0] || '#';
+            document.getElementById(`${day}-exercise2`).textContent = trainingData.exercises[1] || 'No disponible';
+            document.getElementById(`${day}-exercise2-video`).href = trainingData.videos[1] || '#';
+            document.getElementById(`${day}-exercise3`).textContent = trainingData.exercises[2] || 'No disponible';
+            document.getElementById(`${day}-exercise3-video`).href = trainingData.videos[2] || '#';
         }
     });
 };
